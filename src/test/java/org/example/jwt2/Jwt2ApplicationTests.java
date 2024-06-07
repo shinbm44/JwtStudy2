7edbf57a38a810f20de81634d0c77d8a06f654b1
shinbm44 <78016592+shinbm44@users.noreply.github.com>
2024-06-07 09:49:08 +0900

    initial commit

---
+++ src/test/java/org/example/jwt2/Jwt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jwt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
